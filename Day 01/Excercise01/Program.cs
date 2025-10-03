2b6fdc7e85792b587793273b8b6ab771c8f6aa2d
ndthanh1291 <ndthanh1291@gmail.com>
2025-10-03 10:28:16 +0700

    Exercise day 1

---
+++ Day 01/Excercise01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Nhập cân nặng (kg): ");
+double weight = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Nhập chiều cao (m): ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+double bmi = weight / (height * height);
+
+Console.WriteLine($"Chỉ số BMI của bạn là: {bmi:F2}");
